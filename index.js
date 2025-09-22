9f4aec5849175b2fbefdccb0fb999b8d3ea0d255
talha7-bit <talha7.webdev@gmail.com>
2025-09-21 20:17:37 -0700

    /route

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-
+import app from "./app.js"
 import connectdb from "./src/db/db.js"
 import dotenv from "dotenv"
 
 dotenv.config()
 
 connectdb()
-/*
+
+app.get("/",(req,res,next)=>{
+    res.send("working")
+})
+
 const PORT=process.env.PORT || 3000
 
 app.listen(PORT,()=>{
     console.log(`app is running on http://localhost:${PORT}`)
 })
 
-*/
