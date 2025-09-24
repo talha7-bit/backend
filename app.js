c78a46b5e3f427fc195b01a204a16f85b872c739
talha7-bit <talha7.webdev@gmail.com>
2025-09-24 07:17:51 -0700

    slash

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://hmsfp.vercel.app/",
+    origin:"https://hmsfp.vercel.app",
+    maxAge:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
 
