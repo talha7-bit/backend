5d5cc362416ffff50c231f68a5765a6fa712c3f1
talha7-bit <talha7.webdev@gmail.com>
2025-09-24 06:43:02 -0700

    newfurl

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://pfrontend-sigma.vercel.app",
+    origin:"https://hmsfp.vercel.app/",
     credentials:true
 }))
 
