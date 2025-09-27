0932de11454a8847faa2d89740a8168962e52d34
talha7-bit <talha7.webdev@gmail.com>
2025-09-26 21:18:17 -0700

    /

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://hmsf-gray.vercel.app/",
+    origin:"https://hmsf-gray.vercel.app",
     //maxAge:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
