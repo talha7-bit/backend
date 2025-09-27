0314ca2a1d231ab210252aa182112d5afaad9d03
talha7-bit <talha7.webdev@gmail.com>
2025-09-27 07:22:35 -0700

    uifixe

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://frontendhmsproject.vercel.app",
+    origin:"https://frontendhmsp.vercel.app",
     //maxAge:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
