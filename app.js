a9e5a51fa19c291abb20d3410b6c29d28aa61f0b
talha7-bit <talha7.webdev@gmail.com>
2025-09-27 06:29:28 -0700

    newurlf

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://frontendhms.vercel.app",
+    origin:"https://frontendhmsproject.vercel.app",
     //maxAge:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
