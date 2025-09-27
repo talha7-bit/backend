0db381c2cf45dac3d1e22909c1616bcd5a6765d9
talha7-bit <talha7.webdev@gmail.com>
2025-09-27 00:57:12 -0700

    hgk

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://frontendphms.vercel.app",
+    origin:"https://hmsfrontendprojectp.vercel.app",
     //maxAge:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
